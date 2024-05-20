8c725e2491b45a2fb391d7d487510f6c7aa6f474
Kelly Deng <102448307+kellyrdeng@users.noreply.github.com>
2024-05-20 19:44:30 -0400

    Create selection_sort.py

---
+++ selection_sort.py
@@ -0,0 +1,17 @@
+def selection_sort(lst):
+    
+    for i in range(len(lst)): #moves boundary between sorted and unsorted by 1 each time
+        index_of_smallest = i
+        
+        #find smallest element in the unsorted region
+        for j in range(i + 1, len(lst)):
+            if lst[j] < lst[index_of_smallest]:
+                index_of_smallest = j
+        
+        #swap current with the newfound smallest
+        lst[i], lst[index_of_smallest] = lst[index_of_smallest], lst[i]
+    
+    return lst
+
+lst = [3, 2, 1, 5, 7, 1, 2]
+print(selection_sort(lst))
