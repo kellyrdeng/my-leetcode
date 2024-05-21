b53983ccecab5125fa361c79158f6520420c0772
Kelly Deng <102448307+kellyrdeng@users.noreply.github.com>
2024-05-20 20:05:17 -0400

    Update insertion_sort.py

--- algorithm_practice/insertion_sort.py
+++ algorithm_practice/insertion_sort.py
@@ -1,1 +1,16 @@
+def insertion_sort(lst):
+    
+    for i in range(1, len(lst)): #for each element in unsorted
+        
+        #iterate backwards through sorted to find the right place
+        for j in range(i - 1, -1, -1):
+            if lst[i] > lst[j]: #if right place, stop
+                break
+            else: #swap
+                lst[i], lst[j] = lst[j], lst[i]
+                i = j
+                
+    return lst
 
+lst = [8, 9, 2, 10, 27, 1, -80]
+print(insertion_sort(lst))
