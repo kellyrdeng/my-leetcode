ff97c60b0ed454b4a403b329beb116064a8e04c7
Kelly Deng <102448307+kellyrdeng@users.noreply.github.com>
2024-08-04 14:37:19 -0400

    Create 1372_longest_zigzag_path_in_BST.py

---
+++ 1372_longest_zigzag_path_in_BST.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestZigZag(self, root: Optional[TreeNode]) -> int:
+        return max(self.helper(root.left, True, 0), self.helper(root.right, False, 0))
+
+    def helper(self, root, wentLeft, depth):
+        if not root:
+            return depth
+
+        if wentLeft:
+            depth = max(
+                depth,
+                self.helper(root.left, True, 0), #abort current zig zag path
+                self.helper(root.right, False, depth + 1)
+            )
+
+        else: #went right previously
+            depth = max(
+                depth,
+                self.helper(root.left, True, depth + 1),
+                self.helper(root.right, False, 0) #abort current zig zag path
+            )
+
+        return depth
